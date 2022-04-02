ee9da835b9d13b6c05365e184401a680d8363981
Ianhunt <40960588+Ianhunt@users.noreply.github.com>
2022-04-02 14:56:14 +0100

    toh-pt5 finished add dashboard view

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,9 +4,10 @@ import { FormsModule } from '@angular/forms';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { HeroesComponent } from './heroes/heroes.component';
 import { HeroDetailComponent } from './hero-detail/hero-detail.component';
 import { MessagesComponent } from './messages/messages.component';
+import { DashboardComponent } from './dashboard/dashboard.component';
+import { HeroesComponent } from './heroes/heroes.component';
 
 @NgModule({
   declarations: [
@@ -14,6 +15,7 @@ import { MessagesComponent } from './messages/messages.component';
     HeroesComponent,
     HeroDetailComponent,
     MessagesComponent,
+    DashboardComponent,
   ],
   imports: [
     BrowserModule,
@@ -24,3 +26,4 @@ import { MessagesComponent } from './messages/messages.component';
   bootstrap: [AppComponent]
 })
 export class AppModule { }
+``
